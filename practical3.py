76e082bd7a6e70fff789a4080a8be0910f5797e1
SHUBHAM20CE070 <97786549+SHUBHAM20CE070@users.noreply.github.com>
2022-04-16 21:50:15 +0530

    Add files via upload

---
+++ practical3.py
@@ -0,0 +1,13 @@
+#20CE070_Shubham panchal
+#Practical3
+
+n = int(input())
+families = map(int, input().split())
+families = sorted(families)
+for i in range(len(families)):
+    if(i!=len(families)-1):
+        if(families[i]!=families[i - 1] and families[i]!=families[i + 1]):
+            print(families[i])
+        break
+    else:
+        print(families[i])
